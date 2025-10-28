40881d686d633c31bd1b745e53bfd6637d566690
AzurIce <973562770@qq.com>
2025-10-28 08:07:06 +0800

    migrate from suid to kobalte + unocss

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,5 @@
 import 'virtual:uno.css'
 import { render } from "solid-js/web";
 import App from "./App";
-import { createTheme, ThemeProvider } from "@suid/material";
 
-const theme = createTheme({
-  palette: {
-    secondary: {
-      main: "#b6b6b6"
-    }
-  }
-})
-
-render(() => <ThemeProvider theme={theme}>
-  <App />
-</ThemeProvider>, document.getElementById("root") as HTMLElement);
+render(() => <App />, document.getElementById("root") as HTMLElement);
--- uno.config.ts
+++ uno.config.ts
@@ -1,9 +1,11 @@
-import { defineConfig, presetIcons, presetWind4 } from 'unocss'
+import { defineConfig, presetIcons } from 'unocss'
+import { presetWind4 } from '@unocss/preset-wind4'
+import presetPrimitives from 'unocss-preset-primitives'
 
 export default defineConfig({
   presets: [
     presetWind4(),
-    presetIcons()
+    presetIcons(),
+    presetPrimitives()
   ]
-  // ...UnoCSS options
 })
