8700da5b822c647775c215c90cb59cf0798ed422
AzurIce <973562770@qq.com>
2025-11-02 11:32:55 +0800

    使用环境变量设置 BASE_URL

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,21 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 import devtools from 'solid-devtools/vite';
 import UnoCSS from 'unocss/vite'
 
-export default defineConfig(({ mode }) => ({
-  plugins: [devtools(), solidPlugin(), UnoCSS()],
-  server: {
-    port: 3000,
-  },
-  build: {
-    target: 'esnext',
-  },
-  base: mode === 'production' ? '/isw-calculator/' : '/',
-}));
+export default defineConfig(({ mode }) => {
+  // 加载环境变量
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  return {
+    plugins: [devtools(), solidPlugin(), UnoCSS()],
+    server: {
+      port: 3000,
+    },
+    build: {
+      target: 'esnext',
+    },
+    // 优先使用环境变量 BASE_URL，否则根据 mode 决定
+    base: env.BASE_URL || '/',
+  };
+});
